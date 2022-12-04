79114148f06bc93d252e78fd600c3c6c5cba8dc6
Florian Poncelin <fponcelin@me.com>
2022-12-04 09:22:36 +0100

    Minor edits to Day 04 solution

--- Day04/Day04.py
+++ Day04/Day04.py
@@ -4,9 +4,9 @@ part1Count = 0
 part2Count = 0
 
 for line in f :
-    pairs = line.rsplit(',')
-    leftPair = pairs[0].rsplit('-')
-    rightPair = pairs[1].rsplit('-')
+    pairs = line.split(',')
+    leftPair = pairs[0].split('-')
+    rightPair = pairs[1].split('-')
 
     if int(leftPair[0]) >= int(rightPair[0]) and int(leftPair[1]) <= int(rightPair[1]) or int(rightPair[0]) >= int(leftPair[0]) and int(rightPair[1]) <= int(leftPair[1]) :
         part1Count += 1
